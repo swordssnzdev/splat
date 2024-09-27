4e56d929a703ed4bd7b4b33582570d13a7684365
swordssnzdev <swordsscripts@gmail.com>
2024-09-28 00:11:48 +0100

    cleanup (#1)
    cleanup
    
    Delete bush.gd
    
    exclude builds

---
+++ scripts/destructible.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+enum State {OK, DESTROYED}
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var state = State.OK
+
+func _on_area_entered(area: Area2D) -> void:
+	if state == State.OK:
+		sprite_2d.frame = 1
+		audio_stream_player_2d.play()
+		state = State.DESTROYED
