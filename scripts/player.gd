47a0fedda9a0fc8a3ab141954f6ea467b2b5f701
swordssnzdev <swordsscripts@gmail.com>
2024-09-28 19:45:11 +0100

    28 sept (#2)
    * recovery sounds
    
    * add trigger
    
    * add blowing
    
    * multiples
    
    * tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 # State machine
-enum State {DEFAULT, BUILDUP, SNZ_GROUND, SNZ_JUMP, RECOVER}
+enum State {NO_SNZ, DEFAULT, BUILDUP, SNZ_GROUND, RESNZ_GROUND, SNZ_JUMP, RECOVER, BLOW}
 
 const BASE_SPEED = 150.0
 const BASE_JUMP_VELOCITY = -300.0
 const BASE_BUILDUP_LENGTH = 100
 const BASE_SNZ_INTERVAL = 250
 const SNZ_JUMP_VELOCITY = -500.0
+const RESNZ_RATE_ONE_IN_N = 2;
+
+signal trigger
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -16,21 +19,31 @@ const SNZ_JUMP_VELOCITY = -500.0
 @onready var snz_audio_stream_player_2d: AudioStreamPlayer2D = $SnzAudioStreamPlayer2D
 @onready var blow_audio_stream_player_2d: AudioStreamPlayer2D = $BlowAudioStreamPlayer2D
 @onready var buildup_audio_stream_player_2d: AudioStreamPlayer2D = $BuildupAudioStreamPlayer2D
+@onready var recover_audio_stream_player_2d: AudioStreamPlayer2D = $RecoverAudioStreamPlayer2D
+@onready var trigger_audio_stream_player_2d: AudioStreamPlayer2D = $TriggerAudioStreamPlayer2D
 
-var state = State.DEFAULT
+
+var state = State.NO_SNZ
 # Count down to snz
 var buildupProgress = BASE_BUILDUP_LENGTH
 # Count down to start of buildup
 var nextSnzProgress = BASE_SNZ_INTERVAL
 var vrotation = 0
 
+func _ready() -> void:
+	animation_player.play("no_snz")
+
 func goToState(s: State) -> void:
-	if state == s:
+	if state == s && s != State.RESNZ_GROUND:
 		return
 		
 	match s:
+		State.NO_SNZ:
+			animation_player.play("no_snz")
 		State.DEFAULT:
-			nextSnzProgress = BASE_SNZ_INTERVAL
+			if state == State.RECOVER && !snz_audio_stream_player_2d.is_playing():
+				recover_audio_stream_player_2d.play()
+			nextSnzProgress = 0 if state == State.NO_SNZ else BASE_SNZ_INTERVAL
 			animation_player.play("default")
 			pass
 		State.BUILDUP:
@@ -42,7 +55,13 @@ func goToState(s: State) -> void:
 			attacksnz_animation_player.play("spray")
 			animation_player.play("snz")
 			snz_audio_stream_player_2d.play()
-			pass
+		State.RESNZ_GROUND:
+			sprite_2d.flip_h = !sprite_2d.flip_h
+			attacksnz_animation_player.stop()
+			# I wanted particles but they don't work on my computer
+			attacksnz_animation_player.play("spray")
+			animation_player.play("resnz")
+			snz_audio_stream_player_2d.play()
 		State.SNZ_JUMP:
 			# Start spinning
 			vrotation = 1
@@ -53,6 +72,10 @@ func goToState(s: State) -> void:
 			sprite_2d.set_rotation(0)
 			animation_player.play("recover")
 			pass
+		State.BLOW:
+			velocity = Vector2.ZERO
+			animation_player.play("blow")
+			blow_audio_stream_player_2d.play()
 	
 	state = s
 
@@ -68,19 +91,12 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 
 	match state:
+		State.NO_SNZ:
+			walk(direction)
 		State.DEFAULT:
 			nextSnzProgress -= delta * 100
-			
-			# Handle jump.
-			if Input.is_action_just_pressed("jump") and is_on_floor():
-				velocity.y = BASE_JUMP_VELOCITY
-
-			if direction:
-				velocity.x = direction * BASE_SPEED
-			else: # Slow down
-				velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
-				
-			if Input.is_action_just_pressed("debug_snz") || nextSnzProgress <= 0:
+			walk(direction)
+			if nextSnzProgress <= 0 || Input.is_action_just_pressed("debug_snz"):
 				goToState(State.BUILDUP)
 		State.BUILDUP:
 			# Speed up animation as buildup progresses
@@ -91,13 +107,17 @@ func _physics_process(delta: float) -> void:
 			else:
 				velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
 			
-			# For the first 75% of the buildup, play buildup sounds that increase in pitch and volume
-			if !buildup_audio_stream_player_2d.is_playing() && buildupProgress > BASE_BUILDUP_LENGTH * 0.25:
+			# For the first 90% of the buildup, play buildup sounds that increase in pitch and volume
+			if !trigger_audio_stream_player_2d.is_playing() && !buildup_audio_stream_player_2d.is_playing() && buildupProgress > BASE_BUILDUP_LENGTH * 0.1:
 				buildup_audio_stream_player_2d.set_pitch_scale(1 + (BASE_BUILDUP_LENGTH - buildupProgress) / (BASE_BUILDUP_LENGTH * 5))
-				buildup_audio_stream_player_2d.set_volume_db((BASE_BUILDUP_LENGTH - buildupProgress) / BASE_BUILDUP_LENGTH)
+				buildup_audio_stream_player_2d.set_volume_db(5 + 5 * (BASE_BUILDUP_LENGTH - buildupProgress) / BASE_BUILDUP_LENGTH)
 				buildup_audio_stream_player_2d.play()
 			
 			buildupProgress -= delta * 100
+			
+			if direction:
+				sprite_2d.flip_h = velocity.x < 0
+			
 			if buildupProgress <= 0:
 				animation_player.speed_scale = 1 # Reset
 				goToState(State.SNZ_GROUND)
@@ -106,6 +126,8 @@ func _physics_process(delta: float) -> void:
 				velocity.y = SNZ_JUMP_VELOCITY
 				velocity.x = direction * BASE_SPEED
 				goToState(State.SNZ_JUMP)
+		State.RESNZ_GROUND:
+			pass
 		State.SNZ_JUMP:
 			if is_on_floor():
 				goToState(State.RECOVER)
@@ -120,26 +142,61 @@ func _physics_process(delta: float) -> void:
 				sprite_2d.set_rotation(sprite_2d.get_rotation() + vrotation)
 				# Decrease the spin speed
 				vrotation = move_toward(vrotation, 0, 5 * delta)
+				
+				if direction:
+					sprite_2d.flip_h = velocity.x < 0
+					attacksnz_sprite_2d.flip_h = velocity.x < 0
 		State.RECOVER:
 			pass
-		
-	if direction:
-		sprite_2d.flip_h = velocity.x < 0
-		attacksnz_sprite_2d.flip_h = velocity.x < 0
+		State.BLOW:
+			pass
 
+
+	attacksnz_sprite_2d.flip_h = sprite_2d.flip_h
 	move_and_slide()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
 		"snz":
-			match state:
-				State.SNZ_GROUND:
-					goToState(State.RECOVER)
+			end_snz()
+		"resnz":
+			end_snz()
 		"recover":
 			goToState(State.DEFAULT)
+		"blow":
+			goToState(State.NO_SNZ)
+
+func end_snz():
+	if state == State.SNZ_GROUND || state == State.RESNZ_GROUND:
+		if randi() % RESNZ_RATE_ONE_IN_N == 1:
+			goToState(State.RESNZ_GROUND)
+		else:
+			goToState(State.RECOVER)
 
 func die():
-	blow_audio_stream_player_2d.play()
-
-func _on_blow_audio_stream_player_2d_finished():
 	get_tree().reload_current_scene()
+
+func walk(direction):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = BASE_JUMP_VELOCITY
+
+	if direction:
+		sprite_2d.flip_h = velocity.x < 0
+		attacksnz_sprite_2d.flip_h = velocity.x < 0
+		velocity.x = direction * BASE_SPEED
+		animation_player.play()
+	else: # Slow down
+		if sprite_2d.get_frame() == 0 || sprite_2d.get_frame() == 2:
+			animation_player.pause()
+		velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
+		
+	
+	
+	if Input.is_action_just_pressed("blow"):
+		goToState(State.BLOW)
+	
+
+func _on_trigger() -> void:
+	if state == State.NO_SNZ:
+		trigger_audio_stream_player_2d.play()
+		goToState(State.DEFAULT)
