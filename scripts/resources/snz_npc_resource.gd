fffd74d32860260f2ff9cdb57c5c9b6ed7a84d84
swordssnzdev <swordsscripts@gmail.com>
2024-10-27 21:50:58 +0000

    office level wip

---
+++ scripts/resources/snz_npc_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name SnzNpcSpec
+
+@export var texture: Texture = null
+@export var wavs: Array[AudioStream] = []
---
+++ scripts/snz_npc.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var spec: SnzNpcSpec
+@onready var timer: Timer = $Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var snz_audio_stream_player_2d: AudioStreamPlayer2D = $SnzAudioStreamPlayer2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite_2d.set_texture(spec.texture)
+	setRandomAudios(snz_audio_stream_player_2d, spec.wavs)
+	timer.start(randf_range(1, 10))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func setRandomAudios(player: AudioStreamPlayer2D, wavs: Array[AudioStream]):
+	var rand_stream = AudioStreamRandomizer.new()
+	player.set_volume_db(15)
+	player.set_pitch_scale(1)
+	var i = 0
+	for wav: AudioStream in wavs:
+		rand_stream.add_stream(i, wav)
+		i += 1
+	player.set_stream(rand_stream)
+
+func _on_timer_timeout() -> void:
+	if !animation_player.is_playing():
+		animation_player.play("snz")
+		snz_audio_stream_player_2d.play()
+	timer.start(randf_range(1, 10))
